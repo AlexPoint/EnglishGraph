765de52d50c6f27171fddd896d2d33ed2aaca261
Alexandre Point <apoint@fleex.tv>
2015-04-23 16:14:34 +0200

    Commented methods in StringUtilities

--- EnglishGraph/Models/StringUtilities.cs
+++ EnglishGraph/Models/StringUtilities.cs
@@ -17,11 +17,14 @@ namespace EnglishGraph.Models
         private const string NumberPattern = "[\\d\\.,\\s]+";
         private const string OnlyNumbersPattern = "^" + NumberPattern + "$";
         private const string PercentagePattern = "^" + NumberPattern + "%$";
+        private const string WordTokenPattern = "[a-zA-Z0-9]+";
 
         private static readonly string AmountPattern = string.Format("^({0})?{1}({2})?$", string.Join("|", CurrencySymbols),
             NumberPattern, string.Join("|", CurrencySymbols));
 
-
+        /// <summary>
+        /// Whether a string if a number, percentage or amount (with currency)
+        /// </summary>
         public static bool IsFigure(string input)
         {
             if (string.IsNullOrEmpty(input))
@@ -34,6 +37,9 @@ namespace EnglishGraph.Models
                 || Regex.IsMatch(input, AmountPattern);
         }
 
+        /// <summary>
+        /// Whether a string contains only punctuation characters
+        /// </summary>
         public static bool IsPunctuation(string input)
         {
             if (string.IsNullOrEmpty(input))
@@ -44,6 +50,12 @@ namespace EnglishGraph.Models
             return Regex.IsMatch(input, "^\\p{P}+$");
         }
 
+        /// <summary>
+        /// Whether a string is a compound word, ie if it contains a '-' inside the word.
+        /// Ex:
+        /// -test -> not compound
+        /// six-figure -> compound
+        /// </summary>
         public static bool IsCompoundWord(string input)
         {
             if (string.IsNullOrEmpty(input))
@@ -54,7 +66,11 @@ namespace EnglishGraph.Models
             return Regex.IsMatch(input, "^(\\w+\\-)+\\w+$");
         }
 
-        public static bool IsFirstLetterUpperCased(string input)
+        /// <summary>
+        /// Whether the first character of a string is upper cased. 
+        /// Info: if char = '0' or char = '.', IsUpper(char) = false.
+        /// </summary>
+        public static bool IsFirstCharUpperCased(string input)
         {
             if (string.IsNullOrEmpty(input))
             {
@@ -64,6 +80,10 @@ namespace EnglishGraph.Models
             return char.IsUpper(input.First());
         }
 
+        /// <summary>
+        /// Whether the all string is upper cased, 
+        /// ie whether all the letters in the string are upper cased
+        /// </summary>
         public static bool IsAllUpperCased(string input)
         {
             if (string.IsNullOrEmpty(input)) { return false; }
@@ -71,6 +91,9 @@ namespace EnglishGraph.Models
             return input.Where(char.IsLetter).All(char.IsUpper);
         }
 
+        /// <summary>
+        /// Lowercased the first letter of a string
+        /// </summary>
         public static string LowerFirstLetter(string input)
         {
             if (string.IsNullOrEmpty(input))
@@ -79,6 +102,17 @@ namespace EnglishGraph.Models
             }
 
             return char.ToLower(input.First()) + input.Substring(1);
+        }
+
+        /// <summary>
+        /// Whether the input contains at least one letter of number
+        /// Ex:
+        /// - word tokens: 'test', 'O.K.', '12%'
+        /// - non word tokens: '!', ';', '.'
+        /// </summary>
+        public static bool IsWordToken(string input)
+        {
+            return Regex.IsMatch(input, WordTokenPattern);
         }
     }
 }
