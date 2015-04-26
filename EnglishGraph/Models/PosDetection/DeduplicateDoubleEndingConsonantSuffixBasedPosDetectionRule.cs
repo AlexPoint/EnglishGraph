b0bf16c42792441c084e20c9902e19fd5cae4c86
Alex <pointalexandre@gmail.com>
2015-04-26 23:28:03 +0200

    Fix double ending consonant rule exception

--- EnglishGraph/Models/PosDetection/DeduplicateDoubleEndingConsonantSuffixBasedPosDetectionRule.cs
+++ EnglishGraph/Models/PosDetection/DeduplicateDoubleEndingConsonantSuffixBasedPosDetectionRule.cs
@@ -12,6 +12,7 @@ namespace EnglishGraph.Models.PosDetection
             string suffixToDerivedForm, byte deType, byte deRelationshipType, byte derivedFormType)
         {
             this.MatchingCondition = a => a.Token.EndsWith(suffix)
+                && a.Token.Length > suffix.Length + 2
                 // the last two letters must be the same - TODO: include the consonant condition if necessary
                 && a.Token.Substring(0, a.Token.Length - suffix.Length).LastOrDefault() == a.Token.Substring(0, a.Token.Length - suffix.Length).Reverse().Skip(1).FirstOrDefault();
             this.DictionaryEntryCreator = tok => new DictionaryEntry()
